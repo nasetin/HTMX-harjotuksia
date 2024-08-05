1aa4e6dff841a8b4f024a846d5cf2d402bcdc550
nasetin <sasuhujanen@outlook.com>
2024-08-05 19:47:20 +0300

    docker testailu

---
+++ docker_test/src/eka_HTMX/index.php
@@ -0,0 +1,18 @@
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>eks HTMX</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    <header id="main-header">
+        <img src="htmx-logo.png" alt="HTMX logo">
+        <h1>high power tools for HTML</h1>
+    </header>
+    <main>
+        <p>you can build modern user interfaces with the simplicity and power of hypertext</p>
+        <button>Learn More</button>
+    </main>
+</body>
+</html>
